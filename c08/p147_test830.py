0873751ac082375e5b61747f1061415d7a70b45f
moqi <moqimoqidea@gmail.com>
2020-04-05 21:36:53 +0800

    20200405

--- c08/p147_test830.py
+++ c08/p147_test830.py
@@ -4,3 +4,9 @@ shhelf_file = shelve.open('../resource/my_binary_data')
 cats = ['Zophie', 'Pooka', 'Simon']
 shhelf_file['cats'] = cats
 shhelf_file.close()
+
+shhelf_file = shelve.open('../resource/my_binary_data')
+print(type(shhelf_file))
+print(shhelf_file['cats'])
+shhelf_file.close()
+
