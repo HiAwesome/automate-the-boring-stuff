dee1f6e2be029a41580d6ac17b8d9a4985d2994e
moqi <moqimoqidea@gmail.com>
2020-04-06 11:41:29 +0800

    20200406

--- c09/p163_test931.py
+++ c09/p163_test931.py
@@ -2,13 +2,12 @@ import os
 import zipfile
 
 os.chdir('/Users/moqi/Downloads/c09_test/')
-
 zip7file = zipfile.ZipFile('7.zip')
-# 全部解压
-zip7file.extractall()
-# 单个解压
-zip7file.extract('7/zzz.txt')
-# 单个解压到其他地方
-zip7file.extract('7/zzz.txt', '/Users/moqi/Downloads')
+print(zip7file.namelist())
 
+zzz = zip7file.getinfo('7/zzz.txt')
+print(zzz.file_size)
+print(zzz.compress_size)
+
+print('Compressed file is %sx smaller!' % (round(zzz.file_size / zzz.compress_size, 4)))
 zip7file.close()
--- c09/p164_test932.py
+++ c09/p164_test932.py
@@ -2,12 +2,13 @@ import os
 import zipfile
 
 os.chdir('/Users/moqi/Downloads/c09_test/')
+
 zip7file = zipfile.ZipFile('7.zip')
-print(zip7file.namelist())
+# 全部解压
+zip7file.extractall()
+# 单个解压
+zip7file.extract('7/zzz.txt')
+# 单个解压到其他地方
+zip7file.extract('7/zzz.txt', '/Users/moqi/Downloads')
 
-zzz = zip7file.getinfo('7/zzz.txt')
-print(zzz.file_size)
-print(zzz.compress_size)
-
-print('Compressed file is %sx smaller!' % (round(zzz.file_size / zzz.compress_size, 4)))
 zip7file.close()
