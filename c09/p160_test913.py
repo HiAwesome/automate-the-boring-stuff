ce16dcfe81794c0b83289724de0c5441d501fe9a
moqi <moqimoqidea@gmail.com>
2020-04-06 11:17:19 +0800

    20200406

--- c09/p160_test913.py
+++ c09/p160_test913.py
@@ -1,5 +1,4 @@
 import os
-import send2trash
 
 # 删除 /Users/moqi/Downloads/c09_test/9/ 下所有的 txt 文件，不会递归删除
 os.chdir('/Users/moqi/Downloads/c09_test/9/')
@@ -7,6 +6,3 @@ for filename in os.listdir():
     if filename.endswith('.txt'):
         print('These file will be delete: %s.' % filename)
         os.unlink(filename)
-
-# 将文件移动到垃圾桶
-send2trash.send2trash('/Users/moqi/Downloads/c09_test/9/1/1.txt')
