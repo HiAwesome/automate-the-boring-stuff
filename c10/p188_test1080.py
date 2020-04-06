ffdd8ef77230e10fd8e61639d346a05406493c8e
moqi <moqimoqidea@gmail.com>
2020-04-06 21:05:23 +0800

    20200406

--- c10/p188_test1080.py
+++ c10/p188_test1080.py
@@ -1,14 +1,19 @@
-import logging
+import random
 
-# disable 的默认参数是 level=CRITICAL，所以需要禁用所有日志使可以不写参数
-logging.disable()
+guess = ''
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug('Some debugging details.')
-logging.info('The logging module is working.')
-logging.warning('An error message is about to be logged.')
-logging.error('An error has occurred.')
-logging.critical('The program is unable to recover!')
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Tnter heads or tails:')
+    guess = input()
 
-"""
-"""
+toss = ('heads', 'tails')[random.randint(0, 1)]
+
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope, Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
