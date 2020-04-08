3ef6c19046fb17ed2526d573ba7e8522adc63814
moqi <moqimoqidea@gmail.com>
2020-04-08 09:42:06 +0800

    20200408

---
+++ c12/p227_test1251.py
@@ -0,0 +1,19 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+print(wb.sheetnames)
+
+sheet = wb.active
+print(sheet.title)
+
+sheet.title = 'Spam Bacocn Eggs Sheet'
+print(wb.sheetnames)
+
+print('-' * 50)
+
+
+wb = openpyxl.load_workbook('../resource/excel/example.xlsx')
+sheet = wb.active
+sheet.title = 'Spam Spam Spam'
+wb.save('../resource/excel/example_copy.xlsx')
+print('sava success...')
