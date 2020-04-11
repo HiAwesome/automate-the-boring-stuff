3a0c77771f3843353e90aaab5edae46829075563
moqi <moqimoqidea@gmail.com>
2020-04-11 21:21:15 +0800

    20200411

--- c14/p266_test1411.py
+++ c14/p266_test1411.py
@@ -6,6 +6,13 @@ reader = csv.reader(exampleFile)
 data = list(reader)
 
 pprint.pprint(data)
+
+print(data[0][0])
+print(data[0][1])
+print(data[0][2])
+print(data[1][1])
+print(data[6][1])
+
 """
 [['4/5/2014 13:34', 'Apples', '73'],
  ['4/5/2014 3:41', 'Cherries', '85'],
@@ -14,5 +21,9 @@ pprint.pprint(data)
  ['4/10/2014 2:07', 'Apples', '152'],
  ['4/10/2014 18:10', 'Bananas', '23'],
  ['4/10/2014 2:40', 'Strawberries', '98']]
-
+4/5/2014 13:34
+Apples
+73
+Cherries
+Strawberries
 """
