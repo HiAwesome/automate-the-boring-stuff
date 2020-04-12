fd9c0be16cdc4f22251aa7eff60c2da02cdf97a1
moqi <moqimoqidea@gmail.com>
2020-04-12 22:46:06 +0800

    20200412

--- c17/p331_test1721.py
+++ c17/p331_test1721.py
@@ -7,9 +7,19 @@ print(cat.format)
 print(cat.format_description)
 cat.close()
 
+print()
+
+im = Image.new('RGBA', (100, 200), 'purple')
+im.save('../resource/image/output/purpleImage.png')
+im2 = Image.new('RGBA', (20, 20))
+im2.save('../resource/image/output/transparentImage.png')
+print('Output Success')
+
 """
 (816, 1088)
 ../resource/image/zophie.png
 PNG
 Portable network graphics
+
+Output Success
 """
