e1fb7bf877f177065385baa9e498f0b3a9638efb
moqi <moqimoqidea@gmail.com>
2020-04-12 19:26:52 +0800

    20200412

--- c16/p304_test162.py
+++ c16/p304_test162.py
@@ -15,3 +15,12 @@ msg = 'Subject: Hello Tom Smith'
 smtpObj.sendmail(account + '@qq.com', recipient + '@qq.com', msg)
 print('Everything Done.')
 smtpObj.quit()
+
+"""
+(250, b'newxmesmtplogicsvrszc5.qq.com\nPIPELINING\nSIZE 73400320\nSTARTTLS\nAUTH LOGIN PLAIN\nAUTH=LOGIN\nMAILCOMPRESS\n8BITMIME')
+(220, b'Ready to start TLS from 111.197.60.45 to newxmesmtplogicsvrszc5.qq.com.')
+Enter qq number: ********************
+(235, b'Authentication successful')
+Enter recipient qq number: ********************
+Everything Done.
+"""
