8e2e2cf2672173e050bc55e497c60405df92195a
moqi <moqimoqidea@gmail.com>
2020-04-12 21:57:51 +0800

    20200412

--- c16/p319_test1662.py
+++ c16/p319_test1662.py
@@ -1,6 +1,12 @@
-import json, twilio
+import json
+
+from twilio.rest import Client
 
 dic = json.loads(open('/Users/moqi/Documents/Doc/twilio.json').read())
-print(dic)
-print(dic['sid'])
-print(dic['token'])
+sid = dic['sid']
+token = dic['token']
+
+twilioCli = Client(sid, token)
+message = twilioCli.messages.create(body='Just test message', from_=dic['from'], to=dic['to'])
+print(message.sid)
+print('It works, all done.')
