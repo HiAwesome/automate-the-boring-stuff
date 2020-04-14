ae44c31a7dbccdcfa3e8d9c185d5450ab95541d9
moqi <moqimoqidea@gmail.com>
2020-04-14 09:41:22 +0800

    20200414

---
+++ c18/p362_jetbrain_toolbox_update.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+print('click jetbrain toolbox icon')
+pyautogui.click((1314, 11), duration=2)
+
+print('click top right setting button')
+pyautogui.click((1500, 52), duration=2)
+
+print('click check for updates')
+pyautogui.click((1383, 145), duration=2)
+
+print('click projects/tools to return main interface')
+pyautogui.click((1131, 56), duration=2)
