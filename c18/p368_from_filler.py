7b51ef18f756ebf699ffde1031279f6cad34ae12
moqi <moqimoqidea@gmail.com>
2020-04-15 10:20:50 +0800

    20200415

--- c18/p368_from_filler.py
+++ c18/p368_from_filler.py
@@ -5,23 +5,30 @@
 测试问卷地址 https://wj.qq.com/s2/5876762/a132/
 """
 
-import pyautogui, time
+import time
+
+import pyautogui
+
+from c17.c17_image_constant import *
 
 nameField = (619, 775)
 genderField = (663, 911)
-submitField = (890, 1043)
-submitColor = (31, 71, 239)
 pyautogui.PAUSE = 0.5
+
+print('>>> 切屏到 Chrome <<<')
+# 腾讯问卷提交图标
+submitPng = image_path + 'tencent_submit.png'
 
 formData = []
 for i in range(100):
-    formData.append({'name': 'zhangsan' + str(i), 'gender': '男' if i % 2 == 0 else '女'})
+    formData.append({'name': 'zhangsan' + str(i), 'gender': 'man' if i % 2 == 0 else 'woman'})
 
 for person in formData:
-    print('>>> 3 Second pause to let user press ctrl-c <<<')
-    time.sleep(3)
+    print('>>> 1 Second pause to let user press ctrl-c <<<')
+    time.sleep(1)
 
-    while not pyautogui.pixelMatchesColor(submitColor[0], submitColor[1], submitColor[2]):
+    screenBox = pyautogui.locateOnScreen(submitPng)
+    while not screenBox:
         print('Wait until the form page has loaded.')
         time.sleep(0.5)
 
@@ -33,8 +40,13 @@ for person in formData:
     pyautogui.typewrite(person['gender'])
 
     print('Click submit.')
-    pyautogui.click(submitField[0], submitField[1])
+    submitIndex = pyautogui.center(screenBox)
+    (x, y) = submitIndex
+    print(submitIndex)
+    x, y = x // 2, y // 2
+    print('x = %d, y = %d' % (x, y))
+    pyautogui.click(x, y)
 
     print('Refresh page')
-    time.sleep(3)
+    time.sleep(1)
     pyautogui.hotkey('command', 'r')
