7e97207c8bfa549a939e2e007e283418990c1f58
moqi <moqimoqidea@gmail.com>
2020-04-15 09:17:20 +0800

    20200415

--- c18/p363_test1891.py
+++ c18/p363_test1891.py
@@ -1,14 +1,19 @@
 import pyautogui
 
 """
-Hello world Hello worldHello world
+Hello world
 XYab
+print("hello world")!!!
+
 """
 
-
+# 运行时请将 4-7 行删除，以便展示效果
 pyautogui.click(558, 153)
 pyautogui.typewrite('Hello world')
 
 pyautogui.click(558, 179)
 pyautogui.typewrite(['a', 'b', 'left', 'left', 'X', 'Y'])
+
+pyautogui.click(558, 200)
+pyautogui.typewrite('print("hello world")!!!')
 
